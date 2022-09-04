7263431b40ce0039baf1ba2e1372e4f5a001cf1b
Atilla45 <atilla.abdullazade4503@gmail.com>
2022-09-04 14:27:40 -0700

    new

--- static/items/kratos/assets/js/main.js
+++ static/items/kratos/assets/js/main.js
@@ -4,7 +4,7 @@
     window.onscroll = function () {
         var button = document.getElementById('lang_color');
         var header_navbar = document.querySelector(".navbar-area"); var sticky = header_navbar.offsetTop; var logo = document.querySelector('.navbar-brand img')
-        if (window.pageYOffset > sticky) { header_navbar.classList.add("sticky"); logo.src = 'items/kratos/assets/images/logo/logot.png'; button.style.color = "#1956c7" } else { header_navbar.classList.remove("sticky"); logo.src = 'items/kratos/assets/images/logo/logot.png'; }
+        if (window.pageYOffset > sticky) { header_navbar.classList.add("sticky"); logo.src = '/static/items/kratos/assets/images/logo/logot.png'; button.style.color = "#1956c7" } else { header_navbar.classList.remove("sticky"); logo.src = '/static/items/kratos/assets/images/logo/logot.png'; }
         var backToTo = document.querySelector(".scroll-top"); if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) { backToTo.style.display = "flex"; } else { backToTo.style.display = "none"; }
     }; new WOW().init(); let navbarToggler = document.querySelector(".mobile-menu-btn"); navbarToggler.addEventListener('click', function () { navbarToggler.classList.toggle("active"); });
 })();
