1d32deba6058ebd3f569ad85ed6cf393cfc6e6b8
Atilla45 <atilla.abdullazade4503@gmail.com>
2022-09-04 16:44:53 -0700

    signup login about

--- crypto/urls.py
+++ crypto/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path('sign-up', views.SignUp.as_view(), name='signup'),
+    path('login', views.SignIn.as_view(), name='login'),
+    # path('about', views.AboutUs.as_view(), name='about'),
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'home.html'
+
+class SignUp(TemplateView):
+    template_name = 'signup.html'
+
+class SignIn(TemplateView):
+    template_name = 'signin.html'
+
+class AboutUs(TemplateView):
+    template_name = 'about-us.html'
