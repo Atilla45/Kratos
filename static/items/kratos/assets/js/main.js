992efb9c1338ade7733d535b6657dbb2cb4698eb
Atilla45 <atilla.abdullazade4503@gmail.com>
2022-09-06 04:08:31 -0700

    new

--- static/items/kratos/assets/js/main.js
+++ static/items/kratos/assets/js/main.js
@@ -3,6 +3,7 @@
     function fadeout() { document.querySelector('.preloader').style.opacity = '0'; document.querySelector('.preloader').style.display = 'none'; }
     window.onscroll = function () {
         var button = document.getElementById('lang_color');
+        var signup = document.getElementById('sign_color');
         var header_navbar = document.querySelector(".navbar-area"); var sticky = header_navbar.offsetTop; var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) { header_navbar.classList.add("sticky"); logo.src = '/static/items/kratos/assets/images/logo/logot.png'; button.style.color = "#1956c7" } else { header_navbar.classList.remove("sticky"); logo.src = '/static/items/kratos/assets/images/logo/logot.png'; }
         var backToTo = document.querySelector(".scroll-top"); if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) { backToTo.style.display = "flex"; } else { backToTo.style.display = "none"; }
